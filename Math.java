fc1ef0ffcc5ca2980c8d69b64188966ae68639a5
jketlerrene <136633888+jketlerrene@users.noreply.github.com>
2024-02-16 03:07:58 -0500

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class Math1
+{
+   public static void main(String[] args)
+   {
+      double angle = Math.PI;
+      
+      System.out.println(Math.asin(angle));
+   }
+}
