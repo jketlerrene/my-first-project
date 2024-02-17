eed181dbe09dc09a642b8e82ae59832d603b87b5
jketlerrene <136633888+jketlerrene@users.noreply.github.com>
2024-02-17 04:19:14 -0500

    Create MyLinkedList.java

---
+++ MyLinkedList.java
@@ -0,0 +1,73 @@
+class MyLinkedList {
+    ListNode head;
+    int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+    
+    public int get(int index) {
+        if(index < 0 || index >= size)
+            return -1;
+        ListNode current = head;
+        for(int i = 0; i < index; i++){
+            current = current.next;
+        }
+        return current.val;
+    }
+    
+    public void addAtHead(int val) {
+        ListNode newNode = new ListNode(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+    
+    public void addAtTail(int val) {
+        if (head == null) {
+            addAtHead(val);
+            return;
+        }
+        ListNode newNode = new ListNode(val);
+        ListNode current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        size++;
+    }
+    
+    public void addAtIndex(int index, int val) {
+       if (index < 0 || index > size)
+            return;
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        ListNode newNode = new ListNode(val);
+        ListNode current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size)
+            return;
+        if (index == 0) {
+            head = head.next;
+            size--;
+            return;
+        }
+        ListNode current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        current.next = current.next.next;
+        size--;
+    }
+}
